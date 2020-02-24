e975797d9e3a76798209c07e395c709f2262971f
Gabriel Lasso <gabriel.lasso@usp.br>
2020-02-23 23:32:50 -0300

    Create deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+git add -a
+git commit -m "deploy"
+git checkout gh-pages
+git merge master
+nim js -d:deploy main.nim
+git add main.js
+git commit -m "deploy"
+git push origin gh-pages
+git checkout master
+
