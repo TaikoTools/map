251b321571e1534c8930fb9f231ad9961c7ba46f
Gabriel Lasso <gabriel.lasso@usp.br>
2020-03-14 23:01:14 -0300

    Enhancement on nagado SVG

--- src/view/mapView.nim
+++ src/view/mapView.nim
@@ -18,13 +18,13 @@ const svgOkedo="""<svg width="100%" height="100%">
 Sorry, your browser does not support inline SVG.
 </svg>"""
 const svgNagado="""<svg width="100%" height="100%">
-  <ellipse stroke="black" stroke-width="1" fill="#c0c0c0" cx="50%" rx="45%" ry="10%" cy="90%"></ellipse>
-  <ellipse stroke="black" stroke-width="1" fill="#c0c0c0" cx="5%" rx="5%" ry="40%" cy="50%"></ellipse>
-    <ellipse stroke="black" stroke-width="1" fill="#c0c0c0" cx="95%" rx="5%" ry="40%" cy="50%"></ellipse>
-  <ellipse stroke="black" stroke-width="1" fill="#c0c0c0" cx="50%" rx="45%" ry="10%" cy="90%"></ellipse>
-  <rect x="5%" stroke="black" stroke-width="0" fill="#c0c0c0" width="90%" height="80%" y="10%"></rect>
-  <ellipse stroke-width="0" fill="#c0c0c0" cx="50%" rx="45%" ry="10%" cy="90%"></ellipse>
-  <ellipse stroke="black" stroke-width="1" fill="#c0c0c0" cx="50%" rx="45%" ry="10%" cy="10%"></ellipse> 
+  <ellipse stroke="black" stroke-width="1" fill="#c0c0c0" cx="50%" rx="43%" ry="10%" cy="90%"></ellipse>
+  <ellipse stroke="black" stroke-width="1" fill="#c0c0c0" cx="7%" rx="7%" ry="40%" cy="50%"></ellipse>
+    <ellipse stroke="black" stroke-width="1" fill="#c0c0c0" cx="93%" rx="7%" ry="40%" cy="50%"></ellipse>
+  <ellipse stroke="black" stroke-width="1" fill="#c0c0c0" cx="50%" rx="43%" ry="10%" cy="90%"></ellipse>
+  <rect x="7%" stroke="black" stroke-width="0" fill="#c0c0c0" width="84%" height="80%" y="10%"></rect>
+  <ellipse stroke-width="0" fill="#c0c0c0" cx="50%" rx="43%" ry="10%" cy="90%"></ellipse>
+  <ellipse stroke="black" stroke-width="1" fill="#c0c0c0" cx="50%" rx="43%" ry="10%" cy="10%"></ellipse> 
 Sorry, your browser does not support inline SVG.
 </svg>"""
 
